b84c4ee07d2b461d7301ffe08cae1d3ae5d328ec
chenlt <535986983@qq.com>
2023-03-05 22:10:30 +0800

    add .

--- pkg/lc912_排序数组/main.go
+++ pkg/lc912_排序数组/main.go
@@ -5,6 +5,48 @@ func main() {
 
 func sortArray(nums []int) []int {
 	if len(nums) == 0 {
+		return nil
+	}
+
+	buildHeap(nums, (len(nums)-2)/2)
+	heapSort(nums, len(nums)-1)
+	return nums
+}
+
+func heapSort(nums []int, index int) {
+	for i := index; i >= 0; i-- {
+		nums[i], nums[0] = nums[0], nums[i]
+		heapify(nums, i, 0)
+	}
+}
+
+func buildHeap(nums []int, index int) {
+	for i := index; i >= 0; i-- {
+		heapify(nums, len(nums), i)
+	}
+}
+
+func heapify(nums []int, length, index int) {
+	if index >= length {
+		return
+	}
+	c1 := index*2 + 1
+	c2 := index*2 + 2
+	max := index
+	if c1 < length && nums[c1] > nums[max] {
+		max = c1
+	}
+	if c2 < length && nums[c2] > nums[max] {
+		max = c2
+	}
+	if max != index {
+		nums[max], nums[index] = nums[index], nums[max]
+		heapify(nums, length, max)
+	}
+}
+
+func sortArray1(nums []int) []int {
+	if len(nums) == 0 {
 		return []int{}
 	}
 
